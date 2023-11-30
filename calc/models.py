cf48b665c1a868c4128c6beb9d96212b9b789554
prabhakar <prabhakarn@xbi4.com>
2023-11-30 10:51:23 +0530

    Create superUser in django

--- calc/models.py
+++ calc/models.py
@@ -10,3 +10,11 @@ class User(models.Model):
     img= models.ImageField(upload_to='pics')
 
 
+class Customer(models.Model):
+    customer_name = models.CharField(max_length=100)
+    customer_email = models.EmailField(max_length=200)
+    incharge_name = models.CharField(max_length=100)
+    incharge_email = models.EmailField(max_length=200)
+    GSTIN = models.CharField(max_length=100)
+    customer_relationship_start_date= models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
