52279514066b830773d3f143dfeb9d8da9d6a14e
prabhakar <prabhakarn@xbi4.com>
2023-11-30 14:45:42 +0530

    Post func for customer

--- calc/urls.py
+++ calc/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
+from .views import create_customer
 
 
 urlpatterns =[
     path('',views.home,name="home"),
     path("calc",views.myCalc,name="myCalc"),
-    path("add",views.add,name="add")
+    path("add",views.add,name="add"),
+    path("customer",create_customer,name='create_customer')
 ]
 
 
--- calc/views.py
+++ calc/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,request
-
+from  .models import User,Customer
+from .forms import CustomerForm
 # Create your views here.
 
 def home(request):
@@ -8,7 +9,9 @@ def home(request):
 
 
 def myCalc(request):
-    return render(request,'calc2.html',{"name":"Siva"})
+
+    dests = User.objects.all()
+    return render(request,'calc2.html',{"dest":dests})
 
 def add(request):
     val1 = int(request.POST['num1'])
@@ -16,3 +19,13 @@ def add(request):
     res = val1 + val2
 
     return render(request,"result.html",{"Result":res})
+
+def create_customer(request):
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/customer')
+    else:
+        form = CustomerForm()
+    return render(request,'create_customer.html', {'form': form})
